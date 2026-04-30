a8d68214ac085a1b006b2f90119561359373e352
jejeajaaa <djuansetyo26juni@gmail.com>
2026-04-30 18:29:33 +0700

    Add header comments to Mahasiswa.java
    Added header comments with student information.

--- pertemuan7/pertemuan7/src/POLIMORFISME_UNIVERSAL_INCLUSION/NO_4/Mahasiswa.java
+++ pertemuan7/pertemuan7/src/POLIMORFISME_UNIVERSAL_INCLUSION/NO_4/Mahasiswa.java
@@ -1,3 +1,9 @@
+/* NIM  : 24060124140163
+   Nama : Djuan Setyo jati
+   Tgl  : 30 April 2026
+*/
+
+
 package POLIMORFISME_UNIVERSAL_INCLUSION.NO_4;
 
 public class Mahasiswa extends Civitasakademika {
